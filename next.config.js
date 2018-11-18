c5f023b53952f170667832b54ee361d6d23a38a9
ArtemZgh <lowershotower@gmail.com>
2018-11-18 10:49:56 +0300

    docs:- add scss

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 const fetch = require('isomorphic-unfetch')
 
 const withSass = require('@zeit/next-sass')
-module.exports = withSass()
 
-module.exports = {
+module.exports = withSass({
   async exportPathMap () {
     // we fetch our list of posts, this allow us to dynamically generate the exported pages
     const response = await fetch('https://jsonplaceholder.typicode.com/posts?_page=1')
@@ -26,4 +25,5 @@ module.exports = {
       '/': { page: '/' }
     })
   }
-}
+})
+
