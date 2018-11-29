df7cfcde01481cea80348fea841416aa4e10c705
Denis Sidorenko <dongor7@gmail.com>
2018-11-29 11:31:31 +0300

    fix: index page redirection

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,21 @@ import React,{ Component } from 'react'
 import fetch from 'isomorphic-unfetch'
 import "../styles/styles.scss"
 import Link from 'next/link';
+import Router from 'next/router';
 
 export default class extends Component {
 
   static async getInitialProps ({ res }) {
 
-      res.writeHead(302, {
-          Location: '/video/alphavoiceio/'
-      });
-      res.end();
+      if(res){
+          res.writeHead(302, {
+              Location: '/video/alphavoiceio/'
+          });
+          res.end();
+      } else {
+          Router.push('/video/alphavoiceio/');
+      }
+
 
       return {}
 
